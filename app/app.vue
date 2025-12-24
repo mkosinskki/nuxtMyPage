6a6d3645035e033cf213884db89a3d830c8b6829
mkosinski <m.kosinskki@gmail.com>
2025-12-24 13:33:15 +0100

    Added tailwindcss

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
+    <h1 class="text-3xl font-bold underline text-lightMint">
+      Hello world!
+    </h1>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
   </div>
 </template>
